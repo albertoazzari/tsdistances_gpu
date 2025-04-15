e4a653e0ad41c7d59b688ecc2bb68da61c244674
albertoazzari <alberto.azzari@univr.it>
2025-04-15 12:51:56 +0200

    test: enable ERP test with SingleBatchMode implementation

--- src/tests.rs
+++ src/tests.rs
@@ -35,23 +35,23 @@ fn test_device() {
 //     assert!(result.is_ok(), "Failed to run compute shader: {:?}", result);
 // }
 
-// #[test]
-// pub fn test_erp() {
-//     let (device, queue, sba, sda) = crate::utils::get_device();
+#[test]
+pub fn test_erp() {
+    let (device, queue, sba, sda) = crate::utils::get_device();
 
-//     let a = vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0];
-//     let b = vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0];
-//     let gap_penalty = 1.0;
-//     let result = crate::cpu::erp::<MultiBatchMode>(
-//         device.clone(),
-//         queue.clone(),
-//         command_buffer_allocator.clone(),
-//         descriptor_set_allocator.clone(),
-//         &a,
-//         &b,
-//         gap_penalty,
-//     );
+    let a = vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0];
+    let b = vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0];
+    let gap_penalty = 1.0;
+    let result = crate::cpu::erp::<SingleBatchMode>(
+        device.clone(),
+        queue.clone(),
+        sba.clone(),
+        sda.clone(),
+        &a,
+        &b,
+        gap_penalty,
+    );
 
-//     println!("{:?}", result)
-//     // assert_eq!(result, 0.0, "Failed to run ERP: {:?}", result);
-// }
+    println!("{:?}", result)
+    // assert_eq!(result, 0.0, "Failed to run ERP: {:?}", result);
+}
