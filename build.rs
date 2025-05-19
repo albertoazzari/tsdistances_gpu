7fd2ba07af8224708b86d3275cfed83a51b83ffa
Alberto Azzari <alberto.azzari@univr.it>
2025-05-19 15:15:22 +0200

    NOT WORKING Update .gitignore and enable Vulkan memory model in device extensions

--- build.rs
+++ build.rs
@@ -1,115 +1,83 @@
-use rspirv::binary::Assemble;
+use fxhash::FxHashSet;
+use rspirv::{binary::Assemble, dr::Module};
 use spirv_builder::{Capability, MetadataPrintout, SpirvBuilder, SpirvMetadata};
-use spirv_tools::{opt::Optimizer, val::Validator, TargetEnv};
-use std::{env, error::Error, fs};
+use spirv_tools::{
+    binary::Binary,
+    opt::{Optimizer, Passes},
+    val::Validator,
+    Error, TargetEnv,
+};
+use std::{env, fs};
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     if env::var("CARGO_CFG_TARGET_ARCH").unwrap_or_default() == "spirv" {
         return Ok(());
     }
 
-    let mut builder = SpirvBuilder::new(".", "spirv-unknown-vulkan1.2")
-        .print_metadata(MetadataPrintout::Full)
-        .spirv_metadata(SpirvMetadata::None);
-
-    let capabilities = {
-        use spirv_builder::Capability::*;
+    let target_env = TargetEnv::Vulkan_1_2;
+    let validator = spirv_tools::val::create(Some(target_env));
+    let mut optimizer = spirv_tools::opt::create(Some(target_env));
+    let passes = {
+        use Passes::*;
         [
-            Int8,
-            Int64,
-            VulkanMemoryModel,
+            EliminateDeadFunctions,
+            DeadVariableElimination,
+            EliminateDeadConstant,
+            CombineAccessChains,
+            CompactIds,
         ]
     };
-    for c in capabilities {
-        builder = builder.capability(c);
+    for pass in passes {
+        optimizer.register_pass(pass);
     }
+    optimizer.register_performance_passes();
 
-    let compiled = builder.build()?;
-    let spirv_path = compiled.module.unwrap_single();
-    let spirv_module =
-        rspirv::dr::load_bytes(std::fs::read(spirv_path)?).map_err(|e| e.to_string())?;
+    let spirv = SpirvBuilder::new(".", "spirv-unknown-vulkan1.2")
+        .print_metadata(MetadataPrintout::Full)
+        .spirv_metadata(SpirvMetadata::None)
+        .capability(Capability::Int64)
+        .capability(Capability::Int8)
+        .build()?;
+
+    let spirv_module = rspirv::dr::load_bytes(std::fs::read(spirv.module.unwrap_single())?)
+        .map_err(|e| e.to_string())?;
+
+    let entry_fns: FxHashSet<u32> = spirv_module
+        .entry_points
+        .iter()
+        .map(|inst| inst.operands[1].unwrap_id_ref())
+        .collect();
 
     for entry_point in &spirv_module.entry_points {
-        let original_spirv = spirv_module.assemble();
+        let entry_id = entry_point.operands[1].unwrap_id_ref();
+        let execution_mode = spirv_module
+            .execution_modes
+            .iter()
+            .find(|inst| inst.operands.first().unwrap().unwrap_id_ref() == entry_id)
+            .unwrap();
+        let functions = spirv_module
+            .functions
+            .iter()
+            .filter(|f| {
+                let id = f.def.as_ref().unwrap().result_id.unwrap();
+                id == entry_id || !entry_fns.contains(&id)
+            })
+            .cloned()
+            .collect();
 
-        // Run DCE + Performance + Optional Final DCE
-        let optimized_1 = spirv_opt(&original_spirv, SpirvOptKind::DeadCodeElimination)?;
-        let optimized_2 = spirv_opt(&optimized_1.as_words(), SpirvOptKind::Performance)?;
-        let final_optimized =
-            spirv_opt(&optimized_2.as_words(), SpirvOptKind::DeadCodeElimination)?;
-
-        let mut spirv_module =
-            rspirv::dr::load_words(&final_optimized.as_words()).map_err(|e| e.to_string())?;
-
-        spirv_module.capabilities.retain(|inst| {
-            use rspirv::spirv::Capability::*;
-            matches!(
-                inst.operands.first().unwrap().unwrap_capability(),
-                Shader
-                    | VulkanMemoryModel
-                    | Int64
-                    | Int8
-            )
-        });
-
-        let final_spirv = spirv_module.assemble();
-        spirv_val(&final_spirv)?;
-        fs::write(
-            spirv_path.with_extension("spv"),
-            bytemuck::cast_slice(final_spirv.as_slice()),
-        )?;
+        let spirv_module = Module {
+            entry_points: vec![entry_point.clone()],
+            execution_modes: vec![execution_mode.clone()],
+            functions,
+            ..spirv_module.clone()
+        };
+        let spirv = spirv_module.assemble();
+        let optimized = optimizer
+            .optimize(spirv, &mut |_| (), None)
+            .expect("Failed to optimize SPIR-V");
+        validator
+            .validate(optimized, None)
+            .expect("Failed to validate SPIR-V");
     }
-
-    Ok(())
-}
-
-#[derive(Clone, Copy, Debug)]
-enum SpirvOptKind {
-    DeadCodeElimination,
-    Performance,
-}
-
-fn spirv_opt(
-    spirv: &[u32],
-    kind: SpirvOptKind,
-) -> Result<spirv_tools::binary::Binary, Box<dyn Error>> {
-    use spirv_tools::opt::Passes::*;
-    let target_env = TargetEnv::Vulkan_1_2;
-
-    let validator = spirv_tools::val::create(Some(target_env));
-    validator.validate(spirv, None)?;
-
-    let mut optimizer = spirv_tools::opt::create(Some(target_env));
-
-    match kind {
-        SpirvOptKind::DeadCodeElimination => {
-            optimizer.register_pass(StripDebugInfo);
-            optimizer.register_pass(EliminateDeadFunctions);
-            optimizer.register_pass(DeadVariableElimination);
-            optimizer.register_pass(EliminateDeadConstant);
-            optimizer.register_pass(EliminateDeadMembers);
-            optimizer.register_pass(LocalAccessChainConvert);
-            optimizer.register_pass(LocalSingleStoreElim);
-            optimizer.register_pass(LocalMultiStoreElim);
-            optimizer.register_pass(CopyPropagateArrays);
-            optimizer.register_pass(CombineAccessChains);
-            optimizer.register_pass(CompactIds);
-            optimizer.register_pass(UnifyConstant);
-        }
-        SpirvOptKind::Performance => {
-            optimizer.register_pass(StripDebugInfo);
-            optimizer.register_performance_passes();
-            optimizer.register_pass(LocalAccessChainConvert);
-            optimizer.register_pass(CopyPropagateArrays);
-            optimizer.register_pass(CompactIds);
-        }
-    }
-
-    Ok(optimizer.optimize(spirv, &mut |_| (), None)?)
-}
-
-fn spirv_val(spirv: &[u32]) -> Result<(), Box<dyn Error>> {
-    let validator = spirv_tools::val::create(Some(TargetEnv::Vulkan_1_2));
-    validator.validate(spirv, None)?;
     Ok(())
 }
